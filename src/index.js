08b0f401b8c8a7cf5db4f88344a3b31a2efffe29
istvan kosa <istvankosa@ip68-231-154-72.tc.ph.cox.net>
2022-10-10 11:54:48 -0700

    At a stopping point

--- src/index.js
+++ src/index.js
@@ -1,1 +1,34 @@
 console.log('hello from ./src/index.js');
+import './style.css';
+
+function pageLoad() {
+
+  console.log('hello inside pageLoad()')
+
+  let header = document.createElement('header');
+  // console.log(header);
+
+
+  let div = document.createElement('div');
+
+  const h1 = document.createElement('h1');
+  h1.innerHTML = 'Thriver';
+  h1.className = 'h1';
+
+  let p = document.createElement('p');
+  p.innerHTML = 'Food for your Best life.';
+  p.className = 'p';
+
+  let img = document.createElement('img');
+  img.className = 'img';
+  img.src = './restaurant.jpeg';
+  console.log(img);
+  
+  div.appendChild(h1);
+  div.appendChild(p);
+  div.appendChild(img);
+
+  return div;
+}
+
+document.body.appendChild(pageLoad());
